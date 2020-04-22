5be7b0b3ee8169b36a99e7f95eb62d6c6739ac38
Nilesh <ngautam18@gmail.com>
2020-04-22 19:59:39 +0530

    add ed sidebasr

--- application/views/london/layout/sidenavbar.php
+++ application/views/london/layout/sidenavbar.php
@@ -57,9 +57,6 @@
                 </div>
             </div>
         </li>
-
-
-
         <!-- Sidebar Toggler (Sidebar) -->
         <div class="text-center d-none d-md-inline">
             <button class="rounded-circle border-0" id="sidebarToggle"></button>
